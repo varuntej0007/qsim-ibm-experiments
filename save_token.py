dd069afbd2939442e866eb8702a9ad5f3725d2fd
varuntej0007 <133973906+varuntej0007@users.noreply.github.com>
2026-03-29 14:54:17 +0530

    Update save_token.py

--- save_token.py
+++ save_token.py
@@ -2,7 +2,7 @@ from qiskit_ibm_runtime import QiskitRuntimeService
 
 QiskitRuntimeService.save_account(
     channel="ibm_quantum_platform",
-    token="2WNgkMhW0o_c_gFXQRQ6MVZ5M-5mhchsl_pC8dcb1mRC",
+    token="Your_Token",
     overwrite=True
 )
 
